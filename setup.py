fb23bddea55d24ed27e99873afefd4a5893bc2a5
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-08-02 15:26:18 -0400

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ko_contributions',
     version='0.1.dev',
-    packages=['ko_contrib' , 'ko_contrib/scripts' , 'ko_contrib/src'],
+    packages=['ko_contrib/scripts' , 'ko_contrib/src'],
     install_requires = ['biom-format' , 'pandas' , 'seaborn'],
     package_dir={'': 'ko_contrib'},
     url='',
