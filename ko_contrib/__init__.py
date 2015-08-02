2268a6050d9db636fdb0bcb3dbef5dbc8fb2a786
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-08-02 15:23:08 -0400

    structure for packages installation

--- ko_contrib/__init__.py
+++ ko_contrib/__init__.py
@@ -1,1 +1,2 @@
 __author__ = 'xaradrim'
+__add__ = ['ko_circos_utils' , 'ko_contrib']
--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 setup(
     name='ko_contributions',
-    version='0.1',
-    packages=['biom-format , pandas , seaborn '],
+    version='0.1.dev',
+    packages=['ko_contrib' , 'ko_contrib/scripts' , 'ko_contrib/src'],
+    install_requires = ['biom-format' , 'pandas' , 'seaborn'],
     package_dir={'': 'ko_contrib'},
     url='',
     license='',
