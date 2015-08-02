7d75b44074c7a897ec99db1def17a84b7793782b
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-08-02 04:07:13 -0400

    testing the pip option to install the program

--- ko_contrib/ko_circos_utils.py
+++ ko_contrib/ko_circos_utils.py
@@ -6,80 +6,76 @@ Created on Wed Jul 22 16:26:31 2015
 @author: xaradrim
 """
 
+from seaborn import color_palette
 
-import seaborn as sns
-
-
-GET_QUANT_BY_GROUP = lambda x : (x[0] , len(x[1]))
-TO_RGB = lambda x : ( int(x[0]*255) , int(x[1]*255) , int(x[2]*255) )
+GET_QUANT_BY_GROUP = lambda x: (x[0], len(x[1]))
+TO_RGB = lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255))
 
 
-
-def extract_otus_with_taxa(biom_table ,taxa_level = 1):
+def extract_otus_with_taxa(biom_table, taxa_level=1):
     result = {}
-    
+
     for otu in biom_table.ids(axis='observation'):
-        phylum = biom_table.metadata(otu , axis='observation')['taxonomy'][taxa_level]
-        
+        phylum = biom_table.metadata(otu, axis='observation')['taxonomy'][taxa_level]
+
         if phylum in result.keys():
             result[phylum].append(otu)
-            
+
         else:
-            result[phylum] = [otu,]
-    
+            result[phylum] = [otu, ]
+
     return result
 
-def extract_kos_with_taxa(biom_table , path_level = 0):
+
+def extract_kos_with_taxa(biom_table, path_level=0):
     result = {}
-    
+
     for ko in biom_table.ids(axis='sample'):
         pathway_list = biom_table.metadata(ko)['KEGG_Pathways']
-        
-        for pathway in pathway_list: 
-            
+
+        for pathway in pathway_list:
+
             if 'None' in pathway:
                 continue
-            
+
             elif pathway[path_level] in result.keys():
                 result[pathway[path_level]].append(ko)
-                
+
             else:
-                result[pathway[path_level]] = [ko,]
-                
+                result[pathway[path_level]] = [ko, ]
+
     return result
 
 
-def get_values_associated_with_id(table , id , axis = 'observation'):
-        axis_class = 'sample'
-        if axis_class == axis :
-                axis_class = 'observation'
-        print axis_class , axis , "\n"
+def get_values_associated_with_id(table, id, axis='observation'):
+    axis_class = 'sample'
+    if axis_class == axis:
+        axis_class = 'observation'
+    print axis_class, axis, "\n"
 
-        temp = {id : [] }
-        for  i in table.ids(axis_class):
-                if axis_class == 'sample' :
-                        sample = i
-                        obs = id
-                else :
-                        sample = id
-                        obs = i
-                if table.get_value_by_ids(obs , sample) > 0:
-                        temp[id].append(i)
-        return temp
-
-
+    temp = {id: []}
+    for i in table.ids(axis_class):
+        if axis_class == 'sample':
+            sample = i
+            obs = id
+        else:
+            sample = id
+            obs = i
+        if table.get_value_by_ids(obs, sample) > 0:
+            temp[id].append(i)
+    return temp
 
 
-def generate_band_location(biom_table  , taxa_level = 1 , path_level = 0):
-    #extracting the groups and names
-    otus = extract_otus_with_taxa(biom_table , taxa_level)
-    kos = extract_kos_with_taxa(biom_table , path_level)
+def generate_band_location(biom_table, taxa_level=1, path_level=0):
+    # extracting the groups and names
+    otus = extract_otus_with_taxa(biom_table, taxa_level)
+    kos = extract_kos_with_taxa(biom_table, path_level)
 
     # Acq chrm header band information for the groups
-    otu_taxa_group = map(GET_QUANT_BY_GROUP , otus.iteritems())
-    ko_path_group = map(GET_QUANT_BY_GROUP , kos.iteritems())
+    otu_taxa_group = map(GET_QUANT_BY_GROUP, otus.iteritems())
+    ko_path_group = map(GET_QUANT_BY_GROUP, kos.iteritems())
 
-    #Structure for storing the band positioning
+    # Structure for storing the band positioning
 
     '''
     result {
@@ -94,161 +90,147 @@ def generate_band_location(biom_table  , taxa_level = 1 , path_level = 0):
 
     '''
 
-    result = {'otus' : {} , 'keggs' : {} }
-    for taxa , size in otu_taxa_group:
+    result = {'otus': {}, 'keggs': {}}
+    for taxa, size in otu_taxa_group:
         otu_list = otus[taxa]
         index = 0
         count = 0
-        while index < size :
-            result['otus'][otu_list[index]] = (".".join([otu_list[index] , taxa]), count , count+1)
+        while index < size:
+            result['otus'][otu_list[index]] = (".".join([otu_list[index], taxa]), count, count + 1)
             count = count + 2
             index = index + 1
 
-    for path , size in ko_path_group :
+    for path, size in ko_path_group:
         ko_list = kos[path]
         index = 0
         count = 0
-        while index < size :
-            #make the tuple
-            temp = (".".join([ko_list[index] , path.replace(" " , "_")]) , count , count + 1)
+        while index < size:
+            # make the tuple
+            temp = (".".join([ko_list[index], path.replace(" ", "_")]), count, count + 1)
             kegg = ko_list[index]
-            if not kegg in result['keggs'].keys() :
-                result['keggs'][kegg] = [temp,]
+            if not kegg in result['keggs'].keys():
+                result['keggs'][kegg] = [temp, ]
             else:
-                result['keggs'][kegg].append(temp)
-            count = count +2
-            index = index +1
+                list (result['keggs'][kegg]).append(temp)
+            count = count + 2
+            index = index + 1
 
     return result
 
 
-
-
-
-def making_karyotype(biom_table ,output_dir=".", taxa_level = 1 , path_level = 0):
-
+def making_karyotype(biom_table, output_dir=".", taxa_level=1, path_level=0):
     ## chr - ID LABEL START END COLOR
     chrm_tmpl = "chr - %(ID)s %(LABEL)s %(START)s %(END)s %(COLOR)s\n"
     band_tmpl = "band %(PARENT)s %(ID)s %(LABEL)s %(START)s %(END)s %(COLOR)s\n"
-    
-    #extracting the groups and names 
-    otus = extract_otus_with_taxa(biom_table , taxa_level)
-    kos = extract_kos_with_taxa(biom_table , path_level)
-    
+
+    # extracting the groups and names
+    otus = extract_otus_with_taxa(biom_table, taxa_level)
+    kos = extract_kos_with_taxa(biom_table, path_level)
+
     # Acq chrm header band information for the groups
-    otus_chrm = map(GET_QUANT_BY_GROUP , otus.iteritems())
-    kos_chrm = map(GET_QUANT_BY_GROUP , kos.iteritems())        
-    
-    #Makig otu karyotype
+    otus_chrm = map(GET_QUANT_BY_GROUP, otus.iteritems())
+    kos_chrm = map(GET_QUANT_BY_GROUP, kos.iteritems())
+
+    # Makig otu karyotype
     tmpl = {}
-    otu_karyo = open(output_dir+'/otu_karyotype.txt' , "w")
-    for chrm , size  in otus_chrm:
-        tmpl['ID'] = chrm 
+    otu_karyo = open(output_dir + '/otu_karyotype.txt', "w")
+    for chrm, size in otus_chrm:
+        tmpl['ID'] = chrm
         tmpl['LABEL'] = chrm
         tmpl['START'] = 0
-        tmpl['END'] = (size*2)-1 
+        tmpl['END'] = (size * 2) - 1
         tmpl['COLOR'] = chrm
-        line  = chrm_tmpl % tmpl
+        line = chrm_tmpl % tmpl
         otu_karyo.write(line)
-    
-    #add the bands 
-    for chrm , size in otus_chrm:
+
+    # add the bands
+    for chrm, size in otus_chrm:
         otu_list = otus[chrm]
         index = 0
         count = 0
         while index < size:
             tmpl['PARENT'] = chrm
-            tmpl['ID'] =  otu_list[index]
+            tmpl['ID'] = otu_list[index]
             tmpl['LABEL'] = otu_list[index]
             tmpl['START'] = count
-            tmpl['END'] = count+1
+            tmpl['END'] = count + 1
             tmpl['COLOR'] = chrm
-            
+
             count = count + 2
             index = index + 1
-            
-            line  = band_tmpl % tmpl
+
+            line = band_tmpl % tmpl
             otu_karyo.write(line)
     otu_karyo.close()
-    
-    #Making ko karyotype 
-    ko_karyo = open(output_dir+'/ko_karyotype.txt', 'w')
-    for chrm , size  in kos_chrm:
-        chrm = chrm.replace(" " , "_")
-        tmpl['ID'] = chrm 
+
+    # Making ko karyotype
+    ko_karyo = open(output_dir + '/ko_karyotype.txt', 'w')
+    for chrm, size in kos_chrm:
+        chrm = chrm.replace(" ", "_")
+        tmpl['ID'] = chrm
         tmpl['LABEL'] = chrm
         tmpl['START'] = 0
-        tmpl['END'] = (size*2)-1 
+        tmpl['END'] = (size * 2) - 1
         tmpl['COLOR'] = chrm
-        line  = chrm_tmpl % tmpl
+        line = chrm_tmpl % tmpl
         ko_karyo.write(line)
-    
-    #add bands now
-    for chrm , size in kos_chrm:
+
+    # add bands now
+    for chrm, size in kos_chrm:
         # carefull with order in here
-        #chrm needs to be replace but reference in the dict
-        #must be found first then change the reference name
+        # chrm needs to be replace but reference in the dict
+        # must be found first then change the reference name
         ko_list = kos[chrm]
-        chrm = chrm.replace(" " , "_")
+        chrm = chrm.replace(" ", "_")
 
         index = 0
         count = 0
         while index < size:
             tmpl['PARENT'] = chrm
-            tmpl['ID'] = ".".join([ko_list[index] , chrm])
+            tmpl['ID'] = ".".join([ko_list[index], chrm])
             tmpl['LABEL'] = ko_list[index]
 
             tmpl['START'] = count
-            tmpl['END'] = count+1
+            tmpl['END'] = count + 1
             tmpl['COLOR'] = chrm
-            
+
             count = count + 2
             index = index + 1
-            
-            line  = band_tmpl % tmpl
+
+            line = band_tmpl % tmpl
             ko_karyo.write(line)
     ko_karyo.close()
     return
 
 
+def make_color_reference(biom_table, taxa_level=1, path_level=0):
+    ko_quant = map(GET_QUANT_BY_GROUP, extract_kos_with_taxa(biom_table, path_level).iteritems())
+    otu_quant = map(GET_QUANT_BY_GROUP, extract_otus_with_taxa(biom_table, taxa_level).iteritems())
 
-
-def make_color_reference(biom_table , taxa_level = 1 , path_level = 0):
-    ko_quant = map(GET_QUANT_BY_GROUP , extract_kos_with_taxa(biom_table , path_level).iteritems())
-    otu_quant = map(GET_QUANT_BY_GROUP , extract_otus_with_taxa(biom_table , taxa_level).iteritems())
-
-    ko_name , ko_size = zip(*ko_quant)
-    otu_name , otu_size = zip(*otu_quant)
+    ko_name, ko_size = zip(*ko_quant)
+    otu_name, otu_size = zip(*otu_quant)
 
     group = []
     group.extend(ko_name)
     group.extend(otu_name)
 
-
     color_reference = {}
-    colors = sns.color_palette('Set2' , len(group))
+    colors = color_palette('Set2', len(group))
 
-    for member , color  in zip(group , colors):
-        #Names cant have whitespaces between
-        #they must be separated using something else
-        #this case an underscore
+    for member, color in zip(group, colors):
+        # Names cant have whitespaces between
+        # they must be separated using something else
+        # this case an underscore
 
-        member = member.replace(" " , "_")
+        member = member.replace(" ", "_")
         color_reference[member] = TO_RGB(color)
 
     return color_reference
 
-
-    
-
     pass
 
 
-
-
-
-
-def make_circos_conf(biom_table , ko_path , out_path , output_dir):
+def make_circos_conf(biom_table, ko_path, out_path, output_dir):
     templ = '''
 
 ################################################################
@@ -333,26 +315,22 @@ color         = black
 
 
 '''
-    conf_fp = "/".join([output_dir , "circos.conf"])
+    conf_fp = "/".join([output_dir, "circos.conf"])
 
     tmp = ["\n<colors>"]
 
-    for name , color in make_color_reference(biom_table).iteritems():
+    for name, color in make_color_reference(biom_table).iteritems():
         n_color = "{0[0]},{0[1]},{0[2]}".format(color)
-        t = "{} = {} ".format(name , n_color)
+        t = "{} = {} ".format(name, n_color)
         tmp.append(t)
     tmp.append("</colors>\n")
     coloring = "\n".join(tmp)
-    karyo = "karyotype = {},{}\n\n".format(ko_path , out_path)
+    karyo = "karyotype = {},{}\n\n".format(ko_path, out_path)
 
-
-
-
-    doc = open(conf_fp , "w")
+    doc = open(conf_fp, "w")
     doc.write(karyo)
     doc.write(templ)
     doc.write(coloring)
     doc.close()
 
     pass
-
---
+++ ko_contrib/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ko_contributions',
+    version='0.1',
+    packages=['biom-format , pandas , seaborn '],
+    package_dir={'': 'ko_contrib'},
+    url='',
+    license='',
+    author='xaradrim',
+    author_email='',
+    description='version test'
+)
